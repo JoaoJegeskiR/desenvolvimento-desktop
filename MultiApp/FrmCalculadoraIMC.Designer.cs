488a4eee894698377745bf71e2e05a6661a46876
JoaoJegeskiR <joao.lukasrj1@gmail.com>
2025-02-27 21:53:40 -0300

    chore: fazendo a construção do formulario  de IMC

--- MultiApp/FrmCalculadoraIMC.Designer.cs
+++ MultiApp/FrmCalculadoraIMC.Designer.cs
@@ -28,12 +28,83 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.chkAdulto = new System.Windows.Forms.CheckBox();
+            this.chkCriança = new System.Windows.Forms.CheckBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.chkCriança);
+            this.panel1.Controls.Add(this.chkAdulto);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(776, 426);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(18, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Selecione";
+            // 
+            // chkAdulto
+            // 
+            this.chkAdulto.AutoSize = true;
+            this.chkAdulto.BackColor = System.Drawing.Color.White;
+            this.chkAdulto.Checked = true;
+            this.chkAdulto.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkAdulto.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkAdulto.ForeColor = System.Drawing.Color.Gold;
+            this.chkAdulto.Location = new System.Drawing.Point(23, 61);
+            this.chkAdulto.Name = "chkAdulto";
+            this.chkAdulto.Size = new System.Drawing.Size(128, 41);
+            this.chkAdulto.TabIndex = 1;
+            this.chkAdulto.Text = "Adulto";
+            this.chkAdulto.UseVisualStyleBackColor = false;
+            this.chkAdulto.CheckedChanged += new System.EventHandler(this.chkAdulto_CheckedChanged);
+            // 
+            // chkCriança
+            // 
+            this.chkCriança.AutoSize = true;
+            this.chkCriança.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkCriança.ForeColor = System.Drawing.Color.Black;
+            this.chkCriança.Location = new System.Drawing.Point(196, 61);
+            this.chkCriança.Name = "chkCriança";
+            this.chkCriança.Size = new System.Drawing.Size(147, 41);
+            this.chkCriança.TabIndex = 2;
+            this.chkCriança.Text = "Criança";
+            this.chkCriança.UseVisualStyleBackColor = true;
+            this.chkCriança.CheckedChanged += new System.EventHandler(this.chkCriança_CheckedChanged);
+            // 
+            // FrmCalculadoraIMC
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gold;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "FrmCalculadoraIMC";
             this.Text = "FrmCalculadoraIMC";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox chkCriança;
+        private System.Windows.Forms.CheckBox chkAdulto;
     }
 }
--- MultiApp/FrmCalculadoraIMC.cs
+++ MultiApp/FrmCalculadoraIMC.cs
@@ -16,5 +16,21 @@ namespace MultiApp
         {
             InitializeComponent();
         }
+
+        private void chkCriança_CheckedChanged(object sender, EventArgs e)
+        {
+            chkCriança.ForeColor = Color.DarkOrange;
+            chkAdulto.ForeColor = Color.Gray;
+            chkCriança.Checked = false;
+
+        }
+
+        private void chkAdulto_CheckedChanged(object sender, EventArgs e)
+        {
+            chkCriança.ForeColor = Color.Gray;
+            chkAdulto.ForeColor = Color.DarkOrange;
+            chkAdulto.Checked = false;  
+
+        }
     }
 }
