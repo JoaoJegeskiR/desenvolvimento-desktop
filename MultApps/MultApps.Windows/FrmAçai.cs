0adbfacf39d9439599479c86ac8e89636d1b5b48
JoaoJegeskiR <joao.lukasrj1@gmail.com>
2025-03-20 21:52:54 -0300

    feat: começando a fazer Sistema De pedido De Açai. #4

---
+++ MultApps/MultApps.Windows/FrmAçai.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MultApps.Windows
+{
+    public partial class FrmAçai : Form
+    {
+        public FrmAçai()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
