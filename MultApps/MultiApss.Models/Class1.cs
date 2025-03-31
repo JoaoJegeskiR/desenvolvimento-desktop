7bac76ef2dfefef9fefafd13c13284cbf469e5c7
JoaoJegeskiR <joao.lukasrj1@gmail.com>
2025-03-31 20:32:30 -0300

    feat: Adicionar camadas MVC para o projeto.

---
+++ MultApps/MultiApss.Models/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiApss.Models
+{
+    public class Class1
+    {
+    }
+}
